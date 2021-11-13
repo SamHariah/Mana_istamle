13ff592a372df58313152a4f001d50dc14c69f5e
Harish <harish29792@gmail.com>
2021-11-13 12:15:51 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mana_istamle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
