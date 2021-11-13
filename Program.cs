ffe51b6dc99b4290d9bcd97aa409bfd0b34467b4
Rajendra <mrajendra6819@gmail.com>
2021-11-13 12:19:10 +0530

    Added hello

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Mana_istamle
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello");
         }
     }
 }
